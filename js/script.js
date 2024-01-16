5e5207516624aac6916cb5482f21e64477b27dfb
Aarav Juneja (He/Him) <aarav.juneja@ais.amity.edu>
2024-01-16 15:55:19 +0530

    transition

--- js/script.js
+++ js/script.js
@@ -1,27 +1,100 @@
-function updateCountdown() {
-    const countdownElement = document.getElementById("countdown");
-    const countDownDate = new Date("April 20, 2024 00:00:00").getTime();
-    const intervalId = setInterval(updateDisplay, 1000);
-  
-    function updateDisplay() {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-  
-      if (distance < 0) {
-        clearInterval(intervalId);
-        countdownElement.textContent = "MUN Open!";
-        return;
-      }
-  
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-      const formattedCountdown = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-      countdownElement.textContent = formattedCountdown;
-    }
+window.onload = async () => {
+  try {
+    const response = await fetch("https://indiamun-backend.onrender.com/chalja/12ka4", {
+      method: "GET"
+    })
+    console.log("backend initiated")
+  } catch (error) {
+    console.log(error)
   }
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((element) => observer.observe(element));
+
+async function sendData(e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value
+    const subject = document.getElementById("subject").value
+    const text = document.getElementById("text").value
+    if (!email || !subject || !text) {
+      return alert("Please enter all the details.")
+    }
+    console.log(email, subject,text)
+    const data = {
+        "userEmail": email,
+        "subject": subject,
+        "text": text
+    }
+    try {
+        const response = await fetch("https://indiamun-backend.onrender.com/email", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        //   mode: "no-cors",
+          body: JSON.stringify(data),
+        });
+        console.log(JSON.stringify(data))
+        console.log("success")
+        alert("Emails sent successfully. Check your inbox for confirmation")
+      } catch (error) {
+        console.log(error)
+        alert("There was an error in sending the emails. Please try again later.")
+    }
+}
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.getElementsByClassName("specialText").onmouseover = event => {  
+  let iteration = 0;
   
-  updateCountdown();  
+  clearInterval(interval);
   
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
+const hamburger = document.getElementById("hamburger")
+const hamburgerMenu = document.getElementById("hamburgerMenu")
+hamburger.addEventListener('click', (e) => {
+  e.preventDefault()
+  console.log(hamburgerMenu.classList)
+  if (hamburgerMenu.classList.contains("phone-nav-menu-visible")) {
+    hamburgerMenu.classList.add("phone-nav-menu-hidden")
+    hamburgerMenu.classList.remove("phone-nav-menu-visible")
+  } else {
+    hamburgerMenu.classList.add("phone-nav-menu-visible")
+    // hamburgerMenu.classList.remove("phone-nav-menu-visible")
+  }
+})
